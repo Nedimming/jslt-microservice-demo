6f1e2e153426614d73d42081588b42e4d2583f1d
Ned Imming <ned@mazeblasters.com>
2022-10-05 23:41:03 -0700

    remove debug output

--- src/main/java/com/nedwrites/frdemo/JSLTRequestController.java
+++ src/main/java/com/nedwrites/frdemo/JSLTRequestController.java
@@ -3,12 +3,10 @@ package com.nedwrites.frdemo;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 public class JSLTRequestController {
     @PostMapping("/transform")
     public JsonNode transform(@RequestBody JSLTRequest JSLTRequest){
-        System.out.println(JSLTRequest.toString());
         return JSLTRequest.getOutput();
     }
 }
